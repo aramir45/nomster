6f5c98f04a309f8b6832483052159724b0ee125a
aramir45 <aramir33@icloud.com>
2017-10-20 06:19:13 +0000

    section 4.14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,20 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+
 end
 
 
