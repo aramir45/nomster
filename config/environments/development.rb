ec526a646c152c9677c3b5a7a89956c18b0d3448
aramir45 <aramir33@icloud.com>
2017-10-30 18:30:37 +0000

    Lesson 11.42 done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,9 +10,9 @@ Rails.application.configure do
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
-  
+
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
-end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
